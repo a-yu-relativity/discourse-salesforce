a7c7f0ae491b778ebe246b2a49dc64090651463f
Albert Yu <albert.yu@relativity.com>
2018-03-16 14:20:56 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: create-ticket
+# about: A plugin used to create a Salesforce ticket upon topic creation
+# version: 0.0.1
+# authors: Albert Yu
+def test
+    event = DiscourseEvent.new
+end
