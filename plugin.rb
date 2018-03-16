e36c3972bfbe140d72b8bf7836170316219a00b4
Albert Yu <albert.yu@relativity.com>
2018-03-16 17:38:54 -0500

    Add config files

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,8 @@
 # about: A plugin used to create a Salesforce ticket upon topic creation
 # version: 0.0.1
 # authors: Albert Yu
+enabled_site_setting :salesforce_enabled
+
 def test
     event = DiscourseEvent.new
 end
