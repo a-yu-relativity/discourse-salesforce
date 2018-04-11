21764903abcffdc7a7dac5e34236a368c7bd227a
Albert Yu <albert.yu@relativity.com>
2018-04-11 09:22:08 -0500

    Add plugin name

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,10 @@
 # authors: Albert Yu
 enabled_site_setting :salesforce_enabled
 
-def test
-    event = DiscourseEvent.new
+PLUGIN_NAME = "discourse_salesforce".freeze
+
+#register_asset 'stylesheets/salesforce.scss'
+
+after_initialize do
+    
 end
