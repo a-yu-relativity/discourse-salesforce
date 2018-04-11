b96212fb130a01dfb7e6db4cfc811ed89ce1fd06
Albert Yu <albert.yu@relativity.com>
2018-04-11 09:24:54 -0500

    Add url

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,8 @@
 # about: A plugin used to create a Salesforce ticket upon topic creation
 # version: 0.0.1
 # authors: Albert Yu
+# url: https://github.com/a-yu-relativity/discourse-salesforce.git
+
 enabled_site_setting :salesforce_enabled
 
 PLUGIN_NAME = "discourse_salesforce".freeze
